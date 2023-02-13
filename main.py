746dd8721e4deb0610e810fa74f3c95ae91b33d8
ITpapers <vladislaw.boycko@gmail.com>
2023-02-13 12:27:57 -0800

    Second Commit with some basic functionality of Car class

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-
 class Car:
-    print("Rented OUT!!!")
-    def __int__(self):
-        self.cost = 50
-        print("My cost is $50/per day")
+    amount_of_cars = 0
+    def __init__(self, brand="Mers", cost=50):
+        self.cost = cost
+        self.total_value = cost*1000
+        self.color = "Blue"
+        self.brand = brand
+        Car.amount_of_cars += 1
+
+    def __str__(self):
+        return f"{self.brand} is ${self.cost}/per day"
+
+    def __del__(self):
+        print( f"{self.brand} sold")
+
+
+    def rent_out(self):
+        self.total_value = self.total_value - (self.cost * 0.1)
+        print(f"{self.brand} rented out!")
+
+
+
 
 first_car = Car()
+
+second_car = Car("Toyota", 35)
+
+
+print(first_car)
